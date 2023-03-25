6dacb558d805c0429207d068017b53b1b6c03454
YHsu927 <126447998+YHsu927@users.noreply.github.com>
2023-03-26 02:06:13 +0800

    Update _general.py

--- benchmark/cluster/_general.py
+++ benchmark/cluster/_general.py
@@ -26,13 +26,13 @@ def generally_cluster_obs(
     data_name = adata.uns['data_name']
     if isinstance(cl_method, str):
         # load cached clustering results or run clustering
-        cl_data = read_clusters_from_cache(data_name, cl_method, run)
+        cl_data = read_clusters_from_cache(data_name, cl_method, run, k)
         if cl_data is None:
             n_clusters = k
             cl_data = cluster_genes(adata, img, species, cl_method, n_clusters, random_state=random_state+run)
-            write_clusters_as_cache(cl_data, data_name, cl_method, run)
+            write_clusters_as_cache(cl_data, data_name, cl_method, run, k)
     elif callable(cl_method):  # cl_method is a custom functions
-        cl_data = read_clusters_from_cache(adata, cl_method.__name__, run)
+        cl_data = read_clusters_from_cache(adata, cl_method.__name__, run, k)
         if cl_data is None:
             cl_data = cl_method(adata, img, **kwargs) # cl_method must return AnnData
             write_clusters_as_cache(cl_data, cl_method.__name__, run)
